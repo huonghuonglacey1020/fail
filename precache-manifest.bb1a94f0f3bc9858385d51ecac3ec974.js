322c922be7ed0bb247387d11c55109f5810ecca5
huongnguyen <huonghuonglacey1020@gmail.com>
2020-05-17 00:11:35 -0700

    Updates

---
+++ precache-manifest.bb1a94f0f3bc9858385d51ecac3ec974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1436c2ab8aad6de058ed8cd204608537",
+    "url": "/fail/index.html"
+  },
+  {
+    "revision": "06b99e389c4931bfe7c6",
+    "url": "/fail/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c43e2df84ffce8af5a3",
+    "url": "/fail/static/js/2.527aa6a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fail/static/js/2.527aa6a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b99e389c4931bfe7c6",
+    "url": "/fail/static/js/main.24bde9a3.chunk.js"
+  },
+  {
+    "revision": "cd10457c0750d585df28",
+    "url": "/fail/static/js/runtime-main.d725d8b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fail/static/media/logo.5d5d9eef.svg"
+  }
+]);
